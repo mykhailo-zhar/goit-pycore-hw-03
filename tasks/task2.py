11452941dd699435c389ab160e98b9f7d125f294
ihateb335 <zhar.misha@gmail.com>
2026-04-25 12:46:25 +0300

    Refactor get_numbers_ticket

--- tasks/task2.py
+++ tasks/task2.py
@@ -1,5 +1,33 @@
 from random import sample
 
+
+def _validate_input(min: int, max: int, quantity: int) -> bool:
+    """
+    Validate the input parameters.
+
+    Args:
+        min: The minimum possible number in the range.
+        max: The maximum possible number in the range.
+        quantity: The number of numbers to generate.
+    """
+    is_valid = True
+
+    is_valid &= isinstance(min, int)
+    is_valid &= isinstance(max, int)
+    is_valid &= isinstance(quantity, int)
+
+    if not is_valid:
+      return is_valid
+    
+    is_valid &= min >= 1
+    is_valid &= max >= 1
+    is_valid &= max < 1000
+    is_valid &= max >= min
+    is_valid &= quantity >= 1
+
+    return is_valid
+
+
 def get_numbers_ticket(min: int, max: int, quantity: int) -> list[int]:
     """
     Generate a list of unique random numbers within a given range.
@@ -13,23 +41,10 @@ def get_numbers_ticket(min: int, max: int, quantity: int) -> list[int]:
         A list of unique random numbers within the given range.
     """
 
-    if not isinstance(min, int) \
-        or not isinstance(max, int) \
-        or not isinstance(quantity, int):
-      return []
-    
-    if min < 1:
-      return []
-    
-    if max >= 1000 or max < 1:
-      return []
-    
-    if max < min:
-      return []
-    
-    if quantity < 1:
-      return []
 
+    if not _validate_input(min, max, quantity):
+      return []
+    
     if quantity >= max - min + 1:
       return list(range(min, max + 1))
 
