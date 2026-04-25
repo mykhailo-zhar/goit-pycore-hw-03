2ab8c27a0e4e0be14b950ee6fca525796008e4df
ihateb335 <zhar.misha@gmail.com>
2026-04-25 13:25:34 +0300

    Refactor task 2 tests

--- tests/test_task2.py
+++ tests/test_task2.py
@@ -5,53 +5,87 @@ import random
 
 # Invalid input types
 
-def test_invalid_input_types_returns_empty_list():
-    assert get_numbers_ticket('nan', 'nan', 'nan') == []
-    assert get_numbers_ticket('nan', 1, 1) == []
-    assert get_numbers_ticket(1, 'nan', 1) == []
-    assert get_numbers_ticket(1, 1, 'nan') == []
+@pytest.mark.parametrize(
+    "minimum,maximum,quantity",
+    [
+        ('nan', 'nan', 'nan'),
+        ('nan', 1, 1),
+        (1, 'nan', 1),
+        (1, 1, 'nan'),
+    ]
+)
+def test_invalid_input_types_returns_empty_list(minimum, maximum, quantity):
+    assert get_numbers_ticket(minimum, maximum, quantity) == []
 
-# Validation of input parameters
+@pytest.mark.parametrize(
+    "minimum,maximum,quantity",
+    [
+        (-100, 10, 5),
+        (0, 10, 5),
+    ]
+)
+def test_minimum_out_of_range_returns_empty_list(minimum, maximum, quantity):
+    assert get_numbers_ticket(minimum, maximum, quantity) == []
 
-def test_min_out_of_range_returns_empty_list():
-    assert get_numbers_ticket(-100, 10, 5) == []
-    assert get_numbers_ticket(0, 10, 5) == []
+@pytest.mark.parametrize(
+    "minimum,maximum,quantity",
+    [
+        (1, -100, 5),
+        (1, 0, 5),
+        (1, 1000, 5),
+    ]
+)
+def test_maximum_out_of_range_returns_empty_list(minimum, maximum, quantity):
+    assert get_numbers_ticket(minimum, maximum, quantity) == []
 
-def test_max_out_of_range_returns_empty_list():
-    assert get_numbers_ticket(1, -100, 5) == []
-    assert get_numbers_ticket(1, 0, 5) == []
-    assert get_numbers_ticket(1, 1000, 5) == []
-
-def test_min_greater_than_max_returns_empty_list():
+def test_minimum_greater_than_maximum_returns_empty_list():
     assert get_numbers_ticket(10, 1, 5) == []
 
-def test_max_equals_min_returns_list_with_single_element():
+def test_maximum_equals_minimum_returns_list_with_single_element():
     assert get_numbers_ticket(10, 10, 5) == [10]
 
-def test_max_less_than_min_returns_empty_list():
+def test_maximum_less_than_minimum_returns_empty_list():
     assert get_numbers_ticket(10, 1, 5) == []
 
-def test_quantity_out_of_range_returns_empty_list():
-    assert get_numbers_ticket(1, 10, -10) == []
-    assert get_numbers_ticket(1, 10, 0) == []
+@pytest.mark.parametrize(
+    "minimum,maximum,quantity",
+    [
+        (1, 10, -10),
+        (1, 10, 0),
+    ]
+)
+def test_quantity_out_of_range_returns_empty_list(minimum, maximum, quantity):
+    assert get_numbers_ticket(minimum, maximum, quantity) == []
 
 # Validation of output parameters
 
 def test_output_is_list():
-    min, max, quantity = random.randint(-100, 100), \
+    minimum, maximum, quantity = random.randint(-100, 100), \
                          random.randint(-100, 100), \
                          random.randint(-100, 100)
-    assert isinstance(get_numbers_ticket(min, max, quantity), list)
+    assert isinstance(get_numbers_ticket(minimum, maximum, quantity), list)
 
 # Logic
 
-def test_quantity_greater_or_equals_returns_list_with_all_elements():
-   assert len(get_numbers_ticket(1, 10, 10)) == 10
-   assert len(get_numbers_ticket(1, 10, 100)) == 10
+@pytest.mark.parametrize(
+    "minimum,maximum,quantity",
+    [
+        (1, 10, 10),
+        (1, 10, 100),
+    ]
+)
+def test_quantity_greater_or_equals_returns_list_with_all_elements(minimum, maximum, quantity):
+   assert len(get_numbers_ticket(minimum, maximum, quantity)) == min(maximum, quantity)
 
-def test_quantity_less_than_returns_list_with_fewer_elements():
-    assert len(get_numbers_ticket(1, 10, 5)) == 5
-    assert len(get_numbers_ticket(1, 10, 1)) == 1
+@pytest.mark.parametrize(
+    "minimum,maximum,quantity ",
+    [
+        (1, 10, 5),
+        (1, 10, 1),
+    ]
+)
+def test_quantity_less_than_returns_list_with_fewer_elements(minimum, maximum, quantity):
+    assert len(get_numbers_ticket(minimum, maximum, quantity)) == quantity
 
 # Uniqueness of output values
 
